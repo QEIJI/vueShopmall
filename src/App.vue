e4542d1e5d790c4c41eca4762e4b9c0acc7a0c8b
QEIJI <2354774977@qq.com>
2020-11-12 12:04:29 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="shop-container">
+    <keep-alive exclude="ProductDetail">
+        <router-view />
+    </keep-alive>
+    <tab-bar v-show="$store.getters.tabBarShow" />
+</div>
+</template>
+
+<script>
+import TabBar from "@/components/common/tabbar/TabBar";
+
+export default {
+    name: "App",
+    components: {
+        TabBar
+    }
+};
+</script>
